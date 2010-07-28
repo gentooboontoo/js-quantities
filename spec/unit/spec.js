286a1a7d7729577d902a7d5dcc7301498b33de31
Julien Sanchez <julien.sanchez@lim.eu>
2010-07-28 15:18:31 +0200

    Pass unitless and compound create tests

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,10 +1,29 @@
 
 describe 'js-quantities'
   describe 'initialization'
+  
+    it 'should create unitless'
+      qty = new Qty('1')
+      qty.to_f().should.be 1
+      qty.numerator.should.eql ['<1>']
+      qty.denominator.should.eql ['<1>']
+      qty = new Qty('1.5')
+      qty.to_f().should.be 1.5
+      qty.numerator.should.eql ['<1>']
+      qty.denominator.should.eql ['<1>']
+    end
+
     it 'should create simple'
       qty = new Qty('1m')
       qty.scalar.should.be 1
       qty.numerator.should.eql ['<meter>']
+      qty.denominator.should.eql ['<1>']
+    end
+    
+    it 'should create compound'
+      qty = new Qty('1 N*m')
+      qty.scalar.should.be 1
+      qty.numerator.should.eql ['<newton>', '<meter>']
       qty.denominator.should.eql ['<1>']
     end
     
