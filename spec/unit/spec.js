1312fb4405e00b97bfbe102cfe8a873351eda5a5
Julien Sanchez <julien.sanchez@lim.eu>
2010-07-28 12:01:56 +0200

    Start implementing Parse

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,8 +1,11 @@
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'js-quantities'
+  describe 'initialization'
+    it 'should create simple'
+      qty = new Qty('1m')
+      qty.scalar.should.be 1
+      qty.numerator.should.eql ['<meter>']
+      qty.denominator.should.eql ['<1>']
     end
   end
 end
