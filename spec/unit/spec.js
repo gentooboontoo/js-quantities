b6a1dabd4c9af1de0615c5fc2bc576693ce80a60
Julien Sanchez <julien.sanchez@lim.eu>
2010-07-28 14:47:39 +0200

    Pass simple and denominator create tests

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -7,5 +7,13 @@ describe 'js-quantities'
       qty.numerator.should.eql ['<meter>']
       qty.denominator.should.eql ['<1>']
     end
+    
+    it 'should create with denominator'
+      qty = new Qty("1 m/s")
+      qty.scalar.should.be 1
+      qty.numerator.should.eql ['<meter>']
+      qty.denominator.should.eql ['<second>']
+    end
+
   end
 end
