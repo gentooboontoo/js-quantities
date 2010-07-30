de2f41522007c12b59bb7656f11a91b6ce7b57e1
Julien Sanchez <julien.sanchez@lim.eu>
2010-07-30 18:19:18 +0200

    Pass additional tests

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -8,14 +8,13 @@ describe 'js-quantities'
       qty.scalar.should.be 1
     end
 
-  
     it 'should create unitless'
       qty = new Qty('1')
-      qty.to_f().should.be 1
+      qty.toFloat().should.be 1
       qty.numerator.should.eql ['<1>']
       qty.denominator.should.eql ['<1>']
       qty = new Qty('1.5')
-      qty.to_f().should.be 1.5
+      qty.toFloat().should.be 1.5
       qty.numerator.should.eql ['<1>']
       qty.denominator.should.eql ['<1>']
     end
@@ -50,6 +49,10 @@ describe 'js-quantities'
       qty.scalar.should.be 1
       qty.numerator.should.eql ['<meter>','<meter>','<kilogram>','<kilogram>','<joule>','<joule>']
       qty.denominator.should.eql ['<second>','<second>']
+      qty = new Qty("1 m^2/s^2*J^3")
+      qty.scalar.should.be 1
+      qty.numerator.should.eql ['<meter>','<meter>']
+      qty.denominator.should.eql ['<second>','<second>','<joule>','<joule>','<joule>']
     end
 
     it 'should create with zero power'
@@ -64,6 +67,55 @@ describe 'js-quantities'
       qty.scalar.should.be 1
       qty.numerator.should.eql ['<meter>','<meter>']
       qty.denominator.should.eql ['<second>','<second>']
+    end
+
+  end
+
+  describe 'match'
+    it 'should return true with compatible quantities'
+      qty1 = new Qty("1 m*kg/s")
+      qty2 = new Qty("1 in*pound/min")
+      qty1.isCompatible(qty2).should.be_true
+    end
+  end
+
+  describe 'conversion'
+    it 'should convert to base units'
+      qty1 = new Qty("100 cm");
+      qty1.toBase().scalar.should.be 1
+      qty2 = new Qty("1 mm^2 ms^-2");
+      qty2.toBase().scalar.should.be 1
+    end
+
+    it 'should convert to compatible units'
+    end
+  end
+
+  describe 'utility methods'
+    it 'should return kind'
+      qty = new Qty("1 mm")
+      qty.kind().should.be 'length'
+    end
+
+    it 'should know if a quantity is in base units'
+      qty = new Qty("100 cm")
+      qty.isBase().should.be false
+
+      qty = new Qty("1m")
+      qty.isBase().should.be true
+    end
+
+    it 'should return unit part of quantities'
+      qty = new Qty("100 cm")
+      qty.units().should.be "cm"
+      qty = new Qty("100 cm/s")
+      qty.units().should.be "cm/s"
+      qty = new Qty("1 cm^2")
+      qty.units().should.be "cm^2"
+      qty = new Qty("1 cm^2/s^2")
+      qty.units().should.be "cm^2/s^2"
+      qty = new Qty("1 cm^2*J^3/s^2*A^2")
+      qty.units().should.be "cm^2*J^3/s^2*A^2"
     end
   end
 end
