9372e4344bc746fbc148fd0f5f0542aee4334529
Julien Sanchez <julien.sanchez@lim.eu>
2010-08-02 12:06:57 +0200

    Pass compare tests

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -76,18 +76,53 @@ describe 'js-quantities'
       qty1 = new Qty("1 m*kg/s")
       qty2 = new Qty("1 in*pound/min")
       qty1.isCompatible(qty2).should.be_true
+      qty2 = new Qty("1 in/min")
+      qty1.isCompatible(qty2).should.be_false
     end
   end
 
   describe 'conversion'
     it 'should convert to base units'
-      qty1 = new Qty("100 cm");
-      qty1.toBase().scalar.should.be 1
-      qty2 = new Qty("1 mm^2 ms^-2");
-      qty2.toBase().scalar.should.be 1
+      qty = new Qty("100 cm")
+      qty.toBase().scalar.should.be 1
+      qty.toBase().units().should.be "m"
+      qty = new Qty("10 cm")
+      qty.toBase().scalar.should.be 0.1
+      qty.toBase().units().should.be "m"
+      qty = new Qty("1 mm^2 ms^-2")
+      qty.toBase().scalar.should.be 1
+      qty.toBase().units().should.be "m^2/s^2"
     end
 
     it 'should convert to compatible units'
+      //qty = new Qty("1 mm")
+
+    end
+  end
+
+  describe 'comparison'
+    it 'should return true when comparing equal quantities'
+      qty1 = new Qty("1cm")
+      qty2 = new Qty("10mm")
+      qty1.eq(qty2).should.be true 
+    end
+
+    it 'should compare compatible quantities'
+      qty1 = new Qty("1cm")
+      qty2 = new Qty("1mm")
+      qty3 = new Qty("10mm")
+      qty4 = new Qty("28A")
+      qty1.compareTo(qty2).should.be 1
+      qty2.compareTo(qty1).should.be -1
+      qty1.compareTo(qty3).should.be 0
+      -{ qty1.compareTo(qty4) }.should.throw_error "Incompatible quantities"
+
+      qty1.lt(qty2).should.be false
+      qty1.lt(qty3).should.be false
+      qty1.lte(qty3).should.be true
+      qty1.gte(qty3).should.be true
+      qty1.gt(qty2).should.be true
+      qty2.gt(qty1).should.be false
     end
   end
 
