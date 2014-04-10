ca860b528398c5d3e28915a6f8f2f11911d1ed30
Julien Sanchez <julien.sanchez@lim.eu>
2014-04-10 15:33:21 +0200

    Refactor benchmark page and bench array conversion

--- bench/main.js
+++ bench/main.js
@@ -6,22 +6,45 @@ requirejs.config({
   }
 });
 
-(function() {
+require([
+  "src/.committed-quantities",
+  "src/quantities"
+], function(previousQty, currentQty) {
   "use strict";
-  function unitConversion(Qty) {
+
+  function bench(name, func) {
+    JSLitmus.test(name, func);
+  }
+
+  function benchCurrent(name, func) {
+    bench(name + " (current)", func(currentQty));
+  }
+
+  function benchPrevious(name, func) {
+    bench(name + " (GIT)", func(previousQty));
+  }
+
+  function compare(name, func) {
+    benchPrevious(name, func);
+    benchCurrent(name, func);
+  }
+
+  compare("Unit conversion", function(Qty) {
     return function() {
       new Qty("2500 m/h").to("ft/s");
     };
-  }
-
-  require(["src/quantities"], function(Qty) {
-    var convert = unitConversion(Qty);
-    JSLitmus.test("Unit conversion (current)", convert);
   });
 
-  require(["src/.committed-quantities"], function(Qty) {
-    var convert = unitConversion(Qty);
-    JSLitmus.test("Unit conversion (GIT)", convert);
+  compare("Array conversion", function(Qty) {
+    var convert = Qty.swiftConverter("m/h", "ft/s");
+    var values = [];
+    for(var i = 0; i < 1000; i++) {
+      values.push(i);
+    }
+
+    return function() {
+      return convert(values);
+    };
   });
-}());
+});
 
