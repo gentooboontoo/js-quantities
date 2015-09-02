965b88d5e2495189e9d0bc0fe06ad5b3f860fe12
Aaron Glasenapp <aglasenapp@comverge.com>
2015-09-02 11:18:04 -0600

    add aliases to Wh unit and kilo prefix

--- src/quantities.js
+++ src/quantities.js
@@ -46,7 +46,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     "<tera>"  :  [["T","Tera","tera"], 1e12, "prefix"],
     "<giga>"  :  [["G","Giga","giga"], 1e9, "prefix"],
     "<mega>"  :  [["M","Mega","mega"], 1e6, "prefix"],
-    "<kilo>"  :  [["k","kilo"], 1e3, "prefix"],
+    "<kilo>"  :  [["k","K","kilo"], 1e3, "prefix"],
     "<hecto>" :  [["h","Hecto","hecto"], 1e2, "prefix"],
     "<deca>"  :  [["da","Deca","deca","deka"], 1e1, "prefix"],
     "<deci>"  :  [["d","Deci","deci"], 1e-1, "prefix"],
@@ -214,7 +214,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     "<calorie>" :  [["cal","calorie","calories"], 4.18400, "energy",["<meter>","<meter>","<kilogram>"], ["<second>","<second>"]],
     "<Calorie>" :  [["Cal","Calorie","Calories"], 4184.00, "energy",["<meter>","<meter>","<kilogram>"], ["<second>","<second>"]],
     "<therm-US>" : [["th","therm","therms","Therm"], 105480400, "energy",["<meter>","<meter>","<kilogram>"], ["<second>","<second>"]],
-    "<Wh>" : [["Wh"], 3600, "energy",["<meter>","<meter>","<kilogram>"], ["<second>","<second>"]],
+    "<Wh>" : [["Wh", "WH", "wh"], 3600, "energy",["<meter>","<meter>","<kilogram>"], ["<second>","<second>"]],
 
     /* force */
     "<newton>"  : [["N","Newton","newton"], 1.0, "force", ["<kilogram>","<meter>"], ["<second>","<second>"]],
