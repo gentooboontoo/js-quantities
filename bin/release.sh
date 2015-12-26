f172bca6fa5f2e27305a665263a5f9fcd97b158e
Julien Sanchez <julien.sanchez@gmail.com>
2015-12-26 15:16:53 +0100

    Create release script

---
+++ bin/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$(cat RELEASE)
+
+git add History.md README.md bower.json package.json RELEASE
+git commit -m "Release v${VERSION}"
+git tag "v${VERSION}"
+git push origin master
