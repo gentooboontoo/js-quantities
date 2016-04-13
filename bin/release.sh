c3f9aedbe7701a459fdb110b87dbc20c5ea8a995
Julien Sanchez <julien.sanchez@gmail.com>
2016-04-13 10:52:12 +0200

    Push release tag to remote repository

--- bin/release.sh
+++ bin/release.sh
@@ -3,9 +3,10 @@
 set -e
 
 VERSION=$(cat RELEASE)
+TAG=v${VERSION}
 
 git add History.md README.md bower.json package.json RELEASE src/quantities.js
-git commit -m "Release v${VERSION}"
-git tag "v${VERSION}"
-git push origin master
+git commit -m "Release ${TAG}"
+git tag "${TAG}"
+git push origin master "${TAG}"
 npm publish
