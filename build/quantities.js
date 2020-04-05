a1ab24c51e855ea097b929bc929792732d5807d7
Julien Sanchez <julien.sanchez@gmail.com>
2020-04-05 12:52:06 +0200

    Update development dependencies

--- build/quantities.js
+++ build/quantities.js
@@ -24,7 +24,7 @@ SOFTWARE.
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global.Qty = factory());
+  (global = global || self, global.Qty = factory());
 }(this, (function () { 'use strict';
 
   /**
